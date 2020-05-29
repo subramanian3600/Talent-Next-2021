908f64fc5cce3e240276284b870169bf19c487c4
SUBRAMANIAN S V <65941046+subramanian3600@users.noreply.github.com>
2020-05-29 14:02:45 +0530

    Add files via upload

---
+++ Programs_for_Practice/Milestone_1/Lang_basis/cmd_line_arg_3.java
@@ -0,0 +1,10 @@
+class cmd_line_arg_3{
+	public static void main(String[] args){
+		int a;
+		int b;
+		a=Integer.parseInt(args[0]);
+		b=Integer.parseInt(args[1]);
+		
+		System.out.println( a+b);
+	}
+}
